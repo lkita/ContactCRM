fdffe36976d69d5333dc212b1f4bb1fce888802a
John Smith <seahawks567@yahoo.com>
2012-02-16 12:54:03 -1000

    Add Getter and Setter

--- ContactCRM/src/java/contact/Contact.java
+++ ContactCRM/src/java/contact/Contact.java
@@ -3,4 +3,19 @@ package contact;
 public class Contact {
 	String name;
 	String address;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 }
