12164dc8e8aa2c9e9d686d93c0537ba09b880700
John Smith <seahawks567@yahoo.com>
2012-02-20 06:40:51 -1000

    Added Something

--- ContactCRM/src/java/contact/Contact.java
+++ ContactCRM/src/java/contact/Contact.java
@@ -7,5 +7,50 @@ public class Contact {
 	String state;
 	String zip;
 	String specialty;
+	String description;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getSpecialty() {
+		return specialty;
+	}
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
+	
 }
